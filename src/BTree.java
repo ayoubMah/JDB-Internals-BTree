d47462ca23869b8904564948d3a8cd4247e74502
ayoubMah <ayoubelmahjouby9@gmail.com>
2026-02-05 15:03:23 +0100

    ref: separating the index discovery from the action

--- src/BTree.java
+++ src/BTree.java
@@ -2,23 +2,23 @@ public class BTree {
     BTreeNode root;
     int T;
 
-    public BTree(int T){
+    public BTree(int T) {
         this.root = new BTreeNode(T, true);// why true, cuz initially root is leaf with 0 keys
     }
 
     /**
      * Logic described in: docs/level1.md
      */
-    public BTreeNode search(BTreeNode node, int key){
+    public BTreeNode search(BTreeNode node, int key) {
         Integer[] keys = node.keys;
         BTreeNode[] children = node.children;
-        for (int i = 0; i < node.n; i++){
-            if (keys[i] >= key){
-                if (i < node.n && keys[i]== key) return node;
-                else if (node.isLeaf) return null;
-                else return search(children[i], key);
-            }
+        int i = 0;
+        for (int j = 0; j < node.n; j++) {
+            if (keys[i] >= key) break;
+            i++;
         }
-        return null;
+        if (i < node.n && keys[i] == key) return node;
+        else if (node.isLeaf) return null;
+        else return search(children[i], key);
     }
 }
